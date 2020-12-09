0128961b05c8d72e394a10959460e1055a4d2180
Sergei Kompanietc <delightvlg@gmail.com>
2020-12-09 23:29:55 +0300

    added helpers/controllers

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const usersRoutes = require('./routes/users');
+const cardsRoutes = require('./routes/cards');
 
 const app = express();
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', usersRoutes);
+app.use('/', cardsRoutes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
