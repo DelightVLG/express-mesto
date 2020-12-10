583c939208a2ee185e0ca015f0917af18e9a3a01
Sergei Kompanietc <delightvlg@gmail.com>
2020-12-10 20:35:21 +0300

    added pageNotFound 404, eslintrc fixed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const usersRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
+const pageNotFoundRoute = require('./routes/PageNotFound');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const { PORT = 3000 } = process.env;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRoutes);
 app.use('/', cardsRoutes);
+app.use('/', pageNotFoundRoute);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
